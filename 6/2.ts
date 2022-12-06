00e8a1f5f6d31396deae8814cbf71e0e5c712a0b
Paulo Köch <paulo.koch@gmail.com>
2022-12-06 12:08:57 +0000

    6/2

---
+++ 6/2.ts
@@ -0,0 +1,30 @@
+import { input_reader } from "../libtapete.ts";
+
+declare global {
+  interface Array<T> {
+    windows: (size: number) => T[][];
+  }
+}
+
+Array.prototype.windows = function <T>(this: T[], size: number): T[][] {
+  return Array.from(
+    { length: this.length - size + 1 },
+    (_, i) => this.slice(i, i + size),
+  );
+};
+
+const allDifferent = <T>(v: T[]): boolean =>
+  [...(new Set(v)).values()].length == v.length;
+
+const GROUP_SIZE = 14;
+const a = (await input_reader(import.meta.resolve))
+  .split("")
+  .windows(GROUP_SIZE)
+  .findIndex(allDifferent) +
+  GROUP_SIZE;
+
+export default a;
+
+if (import.meta.main) {
+  console.log(a);
+}
